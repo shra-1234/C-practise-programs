e2cc448a6c289523b16362f8d2ac7cb0d221854e
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-02-10 23:44:50 +0530

    Sum of integers

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    // int sum;
+    // sum=a+b;
+    // printf("The sum of a and b is %d",sum);
+    // OR
+    printf("The sum of a and b is %d",a+b);
+    return 0;
+}
