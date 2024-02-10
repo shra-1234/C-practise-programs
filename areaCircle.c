c94e9a5dbb32920774c894f2e91bb32a83e0d52b
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-02-10 23:38:29 +0530

    Calculating area of circle

---
+++ areaCircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float rad;
+    printf("\nEnter the value of radius:");
+    scanf("%f",&rad);
+    float pi=3.14;
+    printf("Area of circle is %f",pi*rad*rad);
+    return 0;
+}
