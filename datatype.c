403386b83fae5446f276f3bc454d1417c91d466d
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-02-10 23:40:51 +0530

    Main datatypes used in C

---
+++ datatype.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // this code explains the types of data
+    int age=22;
+    float pi=3.14;
+    char hastag='#'; 
+    /* this is a multiple 
+    line comment */
+    return 0;
+}
