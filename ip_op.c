d34b85c83c97c0e70665aec41c7c234f43acf217
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-02-10 23:41:47 +0530

    Taking an input and how is output printed

---
+++ ip_op.c
@@ -0,0 +1,23 @@
+ #include<stdio.h>
+ int main(){
+    printf("Hello C \n");
+   //  printf("Hello C \n");
+   //  printf("Hello C \n");
+   //  printf("Hello C \n");
+   //  printf("Hello C \n");
+   //Input 
+   int age=12;
+   char star='8';
+   float height=5.6f;
+   //Output
+   printf("Age is %d\n",age);
+   printf("Star is %c",star);
+   printf("\nHeight is %f",height);
+
+   printf("\n\n\n");
+   printf("Enger your age: ");
+   scanf("%d",&age);
+   printf("\nAge is %d",age);
+
+   return 0;
+ }
