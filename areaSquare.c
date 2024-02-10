d1dea76bd4dcd7365ec4f575d7eec8069c5739bd
Shraddha B Math <117364109+shra-1234@users.noreply.github.com>
2024-02-10 23:40:17 +0530

    Area of a square

---
+++ areaSquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int side;
+    printf("Enter the side of the square : ");
+    scanf("%d",&side);
+    int area;
+    area=side*side;
+    printf("\n Area of the square is : %d",area);
+    return 0;
+
+}
